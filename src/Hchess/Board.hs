8e1b8c20fd72994652474c8f442d5e9839c9dbe6
Chad Gilbert <chad.gilbert@cqlcorp.com>
2014-08-05 13:28:54 -0400

    Added algebraic notation translation

--- src/Hchess/Board.hs
+++ src/Hchess/Board.hs
@@ -1,18 +1,21 @@
-module Hchess.Board (Board(Board),emptyBoard) where
+module Hchess.Board (Board(Board),emptyBoard,toAlgebraic,fromAlgebraic) where
 import qualified Data.Map as Map
+import Data.Char (chr,ord)
 
 data Character = Pawn | Rook | Knight | Bishop | Queen | King
                  deriving (Show)
 
 data Affinity = North | East | South | West
-                   deriving (Show)
+                deriving (Show)
+
+data Team = Team Affinity String deriving (Show)
 
 type Location = (Int,Int)
 
 data Move = Move Location Location Piece
             deriving (Show)
 
-data Piece = Piece Affinity Character [Move]
+data Piece = Piece Team Character [Move]
              deriving (Show)
 
 data Square = Empty | Square Piece
@@ -23,4 +26,13 @@ data Board = Board (Map.Map Location Square)
 
 emptyBoard :: Int -> Int -> Board
 emptyBoard w h = Board (Map.fromList [ ((x,y), Empty) | x <- [0..(w-1)], y <- [0..(h-1)] ])
+
+
+toAlgebraic :: Location -> String
+toAlgebraic (x,y) = charToString (chr (x + 97)) ++ show (y + 1)
+                    where charToString c = [c]
+
+fromAlgebraic :: String -> Location
+fromAlgebraic (x:y) = ((ord x) - 97, (read y :: Int) - 1)
+
 
--- test/Hchess/Board/Test.hs
+++ test/Hchess/Board/Test.hs
@@ -7,9 +7,13 @@ import Hchess.Board
 import Data.Map (size)
 
 boardSuite :: TestTree
-boardSuite = testGroup "Board"
+boardSuite = testGroup "Board" 
     [testCase "board size test" boardSizeTest,
-     testCase "uneven board size test" unevenBoardSizeTest]
+     testCase "uneven board size test" unevenBoardSizeTest,
+     testCase "toAlgebraic" toAlgebraicTest,
+     testCase "toAlgebraic2" toAlgebraicTest2,
+     testCase "fromAlgebraic" fromAlgebraicTest,
+     testCase "fromAlgebraic2" fromAlgebraicTest2]
 
 boardSizeTest :: Assertion
 boardSizeTest = 64 @=? boardSize (emptyBoard 8 8)
@@ -20,3 +24,14 @@ unevenBoardSizeTest = 20 @=? boardSize (emptyBoard 4 5)
 boardSize :: Board -> Int
 boardSize (Board x) = size x
 
+toAlgebraicTest :: Assertion
+toAlgebraicTest = "a1" @=? toAlgebraic (0,0)
+ 
+toAlgebraicTest2 :: Assertion
+toAlgebraicTest2 = "c7" @=? toAlgebraic (2,6)
+
+fromAlgebraicTest :: Assertion
+fromAlgebraicTest = (0,0) @=? fromAlgebraic "a1" 
+ 
+fromAlgebraicTest2 :: Assertion
+fromAlgebraicTest2 = (2,6) @=? fromAlgebraic "c7"
