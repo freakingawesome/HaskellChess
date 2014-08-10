dedb897945fedad9136e141853468c534a80f976
Chad Gilbert <chad.gilbert@cqlcorp.com>
2014-08-09 20:05:16 -0400

    Cleaning up soon-to-be shared convenience methods

--- src/Hchess/Moves.hs
+++ src/Hchess/Moves.hs
@@ -1,11 +1,7 @@
 module Hchess.Moves where
 
 import Hchess.Board
-import qualified Data.Map as Map
-import Data.Char (chr,ord)
-import Data.List (sort)
-import Data.List.Split (splitOn)
-
+import Data.List(sort)
 
 possibleMovesFromLocation :: Board -> Location -> Either String [Location]
 possibleMovesFromLocation b l = possibleMoves b l (pieceAt l b)
@@ -21,22 +17,32 @@ relLoc (x,y) East (r,f) = (x+f,y-r)
 relLoc (x,y) South (r,f) = (x-r,y-f)
 relLoc (x,y) West (r,f) = (x-f,y+r)
 
+-- Shorthand for affinity-relative moves
+fwd :: Location -> Affinity -> Int -> (Int,Int)
+fwd l aff n = relLoc l aff (0,n)
+
+fl :: Location -> Affinity -> Int -> (Int,Int)
+fl l aff n = relLoc l aff (-1 * n,n)
+
+fr :: Location -> Affinity -> Int -> (Int,Int)  
+fr l aff n = relLoc l aff (n,n)
+
+-- List out all possible moves per piece
 possibleMovesByPiece :: Board -> Location -> Piece -> [Location]
 
 -- Pawn
 possibleMovesByPiece (Board m capt) l (Piece (Team aff t) Pawn ls) =
   let 
     straight = if ls == [] then 
-      lineOfSightUnoccupied (Board m capt) [fwd 1,fwd 2] 
+      lineOfSightUnoccupied (Board m capt) [fwd' 1,fwd' 2] 
     else 
-      filterUnoccupied (Board m capt) [fwd 1]
-    diag = filterOccupiedByEnemy (Board m capt) (Team aff t) [fl 1, fr 1]
+      filterUnoccupied (Board m capt) [fwd' 1]
+    diag = filterOccupiedByEnemy (Board m capt) (Team aff t) [fl' 1, fr' 1]
   in sort (straight ++ diag)
-  where 
-    fwd n = relLoc l aff (0,n)
-    fl n = relLoc l aff (-1 * n,n)
-    fr n = relLoc l aff (n,n)
-
+  where
+    fwd' = fwd l aff
+    fl'  = fl l aff
+    fr'  = fr l aff
 
 -- One last catch-all for unknown pieces
 possibleMovesByPiece _ _ p = error ("Piece not yet handled: " ++ (show p))
