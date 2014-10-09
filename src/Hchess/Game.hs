f67190cf23da4f6a72b07c5bcbb2f4e66d95714e
Chad Gilbert <chad.gilbert@cqlcorp.com>
2014-10-08 22:55:38 -0400

    Added circular structure for teams and turns

--- src/Hchess/Game.hs
+++ src/Hchess/Game.hs
@@ -6,12 +6,12 @@ import qualified Data.Map as Map
 import Data.Either.Unwrap
 import Data.Maybe
 
-data Game = Game Board Team
+data Game = Game Board [Team]
 
 newGame :: Int -> Int -> [(Team,String)] -> Game
 newGame _ _ [] = error "You must have at least two teams"
 newGame _ _ [t] = error "You must have at least two teams"
-newGame w h teamPlacement = Game (newBoard w h teamPlacement) (fst (head teamPlacement))
+newGame w h teamPlacement = Game (newBoard w h teamPlacement) (getTurns (map (\(t,s) -> t) teamPlacement))
 
 newStandardGame :: Game
 newStandardGame =
@@ -19,15 +19,40 @@ newStandardGame =
     white = Team North "White"
     black = Team South "Black"
   in
-    Game (newStandardBoard white black) white
+    Game (newStandardBoard white black) [white,black]
 
 performMove :: Game -> (Location,Location) -> Maybe Character -> Either String Game
-performMove (Game b curTeam) (from,to) promo
+performMove (Game b teams) (from,to) promo
   | isLeft fromContents = Left "Invalid location"
+  | isLeft pms = Left "Illegal move" 
   | isNothing fromSquare = Left "The source square is empty"
-  | otherwise = Right (Game b curTeam)
+  | not (to `elem` possibleTargetLocs) = Left "Illegal move"
+  | otherwise = Right (Game b teams)
   where
     fromContents = pieceAt from b
     fromSquare = fromRight fromContents
     pms = possibleMovesFromLocation b from (length (remainingTeams b)) 
+    possibleTargetLocs = map (\(Move (_,to') _) -> to') (fromRight pms)
     
+getTurns :: Ord x => [x] -> [x]
+getTurns [] = []
+getTurns x = x ++ (getTurns x)
+
+turnsToTeams :: Ord x => [x] -> [x]
+turnsToTeams [] = []
+turnsToTeams (x:xs) = x : turnsToTeams' xs
+  where 
+    turnsToTeams' (x':xs')
+      | x' == x = []
+      | otherwise = x' : turnsToTeams' xs' 
+
+teamCount :: Ord x => [x] -> Int
+teamCount x = length (turnsToTeams x)
+
+removeTeam :: Ord x => [x] -> x -> [x]
+removeTeam [] _ = []
+removeTeam (x:xs) r
+  | x == r && head xs == r = []
+  | x == r = removeTeam xs r
+  | otherwise = x : removeTeam xs r
+
--- test/Hchess/Game/GameSpec.hs
+++ test/Hchess/Game/GameSpec.hs
@@ -7,7 +7,24 @@ import Data.List(sort)
 
 spec :: Spec
 spec = do
-  return ()
-  -- describe "Dummy describe" $ do
-    -- it "can move to either d3 or d4 if isolated" $ do
-      -- True `shouldBe` True
+  describe "Team management" $ do
+    it "should repeat the team order when cycling through" $ do
+      take 10 (getTurns [1,2,3]) `shouldBe` [1,2,3,1,2,3,1,2,3,1]
+
+    it "should get an appropriate team count for one team" $ do
+      teamCount (getTurns [1]) `shouldBe` 1
+
+    it "should get an appropriate team count for two teams" $ do
+      teamCount (getTurns [1,2]) `shouldBe` 2
+
+    it "should get an appropriate team count for many teams" $ do
+      teamCount (getTurns [1,2,3,4,5,6]) `shouldBe` 6
+
+    it "should remove teams appropriately" $ do
+      take 10 (removeTeam (getTurns [1,2,3]) 2) `shouldBe` [1,3,1,3,1,3,1,3,1,3]
+
+    it "should be able to change teams back into turns at the first position" $ do
+      turnsToTeams (getTurns [1,2,3]) `shouldBe` [1,2,3]
+
+    it "should be able to change teams back into turns at other positions" $ do
+      turnsToTeams (tail (getTurns [1,2,3])) `shouldBe` [2,3,1]
