cc3259aa38d3ac32d88f96633bb51d9a4018581b
Chad Gilbert <chad.gilbert@cqlcorp.com>
2014-10-18 15:13:26 -0400

    You can actually play now!

--- src/Main.hs
+++ src/Main.hs
@@ -10,20 +10,72 @@ import Hchess.Moves
 import Hchess.Game
 
 import Data.Maybe(fromJust)
--- import Data.List(sort)
+import Data.List(sort)
+import Data.List.Split(splitOn)
 import Data.Either.Unwrap
+import Text.Regex.Posix
 
 main :: IO ()
 main = do
-  putStrLn (utf8Board b)
+  putStrLn (utf8Game g)
+  getUserInput g
+  return ()
+  where g = newStandardGame
+
+getUserInput :: Game -> IO ()
+getUserInput g = do
+  input <- getLine
+  case head (splitOn " " input) of
+    "?" -> showHelp
+    "help" -> showHelp
+    "exit" -> putStrLn "Goodbye"
+    "board" -> do
+      putStrLn (utf8Game g)
+      getUserInput g
+    "mv" ->
+      let
+        (_,_,_,mvs) = input =~ "^mv ([a-h][1-8]) ([a-h][1-8])$" :: (String,String,String,[String])
+      in do
+      if null mvs then do
+        putStrLn "Parse error. Should be something like: mv a2 a3"
+        getUserInput g
+      else
+        let
+          (from,to) = (fromAlgebraicLocation (head mvs),fromAlgebraicLocation (mvs!!1))
+        in
+          let
+            mv = performMove g (from,to) Nothing
+          in
+            if isLeft mv then do
+              putStrLn (fromLeft mv)
+              getUserInput g
+            else do
+              putStrLn (utf8Game (fromRight mv))
+              getUserInput (fromRight mv)
+    "pm" -> do
+      putStrLn "Show possible moves"
+      getUserInput g
+    _ -> do
+      putStrLn "Huh?"
+      getUserInput g
+  return ()
   where
-    Game b _ = newStandardGame
+    showHelp = do
+      putStrLn "\n\
+        \When you see <loc>, use algebraic notation. For example, a1 is the lower left\n\
+        \corner and h8 is the upper right.\n\n\
+        \pm <loc>         Shows possible move from location\n\
+        \mv <loc> <loc>   Attempts to move the piece at the first loc to the second\n"
+      getUserInput g
+
+utf8Game :: Game -> String
+utf8Game (Game b (cur:_)) = utf8Board b ++ "\nIt is " ++ teamName cur ++ "'s turn\n"
 
 utf8Board :: Board -> String
-utf8Board (Board m c bs) = boardRows (Board m c bs) length height
+utf8Board (Board m c bs) = boardRows (Board m c bs) len hgt
   where
     locs = map fst (Map.toList m)
-    (length,height) = maximum locs
+    (len,hgt) = maximum locs
 
 boardRows :: Board -> Int -> Int -> String
 boardRows _ _ (-1) = ""
