ab979804b99adad6f826bbd9c2b6a2a1ceaa65e3
Chad Gilbert <chad.gilbert@cqlcorp.com>
2015-09-23 19:39:55 -0400

    Refactor in prep for FEN parsing

--- test/Hchess/Fen/FenSpec.hs
+++ test/Hchess/Fen/FenSpec.hs
@@ -18,35 +18,52 @@ spec = do
     let
       mv game from to = fromRight $ performMove game ((fromAlgebraicLocation from),(fromAlgebraicLocation to)) Nothing
       wikipediaMove0 = newStandardGame
+      fenMove0 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
       wikipediaMove1 = mv wikipediaMove0 "e2" "e4"
+      fenMove1 = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
+
       wikipediaMove2 = mv wikipediaMove1 "c7" "c5"
+      fenMove2 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
+
       wikipediaMove3 = mv wikipediaMove2 "g1" "f3"
-      myMove4        = mv wikipediaMove3 "e7" "e5"
-      myMove5        = mv myMove4 "f3" "e5"
-      myMove6        = mv myMove5 "e8" "e7"
-      myMove7        = mv myMove6 "e1" "e2"
+      fenMove3 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
 
-    it "should have the correct FEN output for a new game" $ do
-      toFen wikipediaMove0 `shouldBe` "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+      myMove4 = mv wikipediaMove3 "e7" "e5"
+      fenMove4 = "rnbqkbnr/pp1p1ppp/8/2p1p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq e6 0 3"
 
-    it "should have the correct FEN output after move e4" $ do
-      toFen wikipediaMove1 `shouldBe` "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
+      myMove5 = mv myMove4 "f3" "e5"
+      fenMove5 = "rnbqkbnr/pp1p1ppp/8/2p1N3/4P3/8/PPPP1PPP/RNBQKB1R b KQkq - 0 3"
 
-    it "should have the correct FEN output after move c5" $ do
-      toFen wikipediaMove2 `shouldBe` "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
+      myMove6 = mv myMove5 "e8" "e7"
+      fenMove6 = "rnbq1bnr/pp1pkppp/8/2p1N3/4P3/8/PPPP1PPP/RNBQKB1R w KQ - 1 4"
 
-    it "should have the correct FEN output after move Nf3" $ do
-      toFen wikipediaMove3 `shouldBe` "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
+      myMove7 = mv myMove6 "e1" "e2"
+      fenMove7 = "rnbq1bnr/pp1pkppp/8/2p1N3/4P3/8/PPPPKPPP/RNBQ1B1R b - - 2 4"
 
-    it "should have the correct FEN output after move e5" $ do
-      toFen myMove4 `shouldBe` "rnbqkbnr/pp1p1ppp/8/2p1p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq e6 0 3"
+    describe "should have the correct FEN output" $ do
 
-    it "should have the correct FEN output after capture at e5" $ do
-      toFen myMove5 `shouldBe` "rnbqkbnr/pp1p1ppp/8/2p1N3/4P3/8/PPPP1PPP/RNBQKB1R b KQkq - 0 3"
+      it "for a new game" $ do
+        toFen wikipediaMove0 `shouldBe` fenMove0
 
-    it "should have the correct FEN output (no castling for black) after king moves to e7" $ do
-      toFen myMove6 `shouldBe` "rnbq1bnr/pp1pkppp/8/2p1N3/4P3/8/PPPP1PPP/RNBQKB1R w KQ - 1 4"
+      it "after move e4" $ do
+        toFen wikipediaMove1 `shouldBe` fenMove1
 
-    it "should have the correct FEN output (no castling for either) after king moves to e2" $ do
-      toFen myMove7 `shouldBe` "rnbq1bnr/pp1pkppp/8/2p1N3/4P3/8/PPPPKPPP/RNBQ1B1R b - - 2 4"
+      it "after move c5" $ do
+        toFen wikipediaMove2 `shouldBe` fenMove2
+
+      it "after move Nf3" $ do
+        toFen wikipediaMove3 `shouldBe` fenMove3
+
+      it "after move e5" $ do
+        toFen myMove4 `shouldBe` fenMove4
+
+      it "after capture at e5" $ do
+        toFen myMove5 `shouldBe` fenMove5
+
+      it "after king moves to e7 (no castling for black)" $ do
+        toFen myMove6 `shouldBe` fenMove6
+
+      it "after king moves to e2 (no castling for either)" $ do
+        toFen myMove7 `shouldBe` fenMove7
 
